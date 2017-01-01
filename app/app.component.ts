eda9859bfe91b9003b36ab66b352b6039dd2c899
Adam Kariv <adam.kariv@gmail.com>
2017-01-02 00:25:47 +0200

    README + some design fixes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  encapsulation: ViewEncapsulation.None,
   styles: [`
-    budgetkey-container {
+    budgetkey-container > .app {
         background-color: #ccc;
         display: block;
     }
