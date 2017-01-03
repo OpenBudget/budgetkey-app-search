2a479994c61e5b4ab967de1668372bf7bd3cf243
Mushon Zer-Aviv <mushon@shual.com>
2017-01-03 10:30:10 +0200

    more styling to search item

--- app/search_result_budget.component.ts
+++ app/search_result_budget.component.ts
@@ -8,39 +8,6 @@ import {DocResultEntry} from "./SearchResults";
     moduleId: module.id,
     selector: 'search-result-budget',
     template: require('./search_result_budget.component.html!text'),
-    styles: [`
-        .item {
-            background: white;
-            margin: 2px 20px;
-            padding: 4px 10px;
-        }
-
-        .header {
-            display: block;
-            width: 100%;
-        }
-
-        .code {
-            color: #999;
-            font-size: 1em;
-        }
-
-        .title {
-            font-weight: bold;
-            color: #000;
-            font-size: 1.5em;
-        }
-
-        .amount {
-        }
-
-        .details {
-            display: block;
-            width: 100%;
-            color: #999;
-        }
-     `
-    ]
 })
 export class SearchResultBudgetComponent implements OnInit {
 
