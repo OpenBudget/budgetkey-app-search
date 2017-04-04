ac3a0dde202e54e011d0f40e059cba074065d0a1
yonti <yontilevin@gmail.com>
2017-04-04 09:01:15 +0300

    reversed last merge

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,5 +8,8 @@ describe('QuickStart E2E Tests', function () {
     browser.get('');
   });
 
+  it('should display: ' + expectedMsg, function () {
+   expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+ });
 
 });
