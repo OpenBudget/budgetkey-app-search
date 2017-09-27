a137fb04b1e40b607633589dbbc8730cd06a85b0
Levko Kravets <levko.ne@gmail.com>
2017-09-27 11:53:07 +0300

    OpenBudget/budgetkey-app-search/issues/75

--- karma-test-shim.ts
+++ karma-test-shim.ts
@@ -1,11 +1,16 @@
 /* global jasmine, __karma__, window, Error */
 
-require('core-js'); // ES6 + reflect-metadata
-require('zone.js/dist/zone');
-require('zone.js/dist/proxy');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/async-test');
-require('zone.js/dist/jasmine-patch');
+declare let window: any;
+
+if (!window.__$zone$Loaded) {
+  require('core-js'); // ES6 + reflect-metadata
+  require('zone.js/dist/zone');
+  require('zone.js/dist/proxy');
+  require('zone.js/dist/sync-test');
+  require('zone.js/dist/async-test');
+  require('zone.js/dist/jasmine-patch');
+  window.__$zone$Loaded = true;
+}
 
 import { TestBed } from '@angular/core/testing';
 import {
