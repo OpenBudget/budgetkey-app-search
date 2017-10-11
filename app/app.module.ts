3044e6e1a48bbc390302fb145200ba85b946c661
Adam Kariv <akariv@users.noreply.github.com>
2017-10-12 01:12:39 +0300

    Major refactor and redesign (#84)

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,15 +9,8 @@ import { SearchService } from './_service/search.service';
 import { SearchComponent } from './search/search.component';
 
 import { BudgetKeyCommonModule } from 'budgetkey-ng2-components';
-import { SearchResultBudgetComponent } from './search_result/search_result.component';
-import { SearchResultChangesComponent } from './search_result/search_result.component';
-import { SearchResultExemptionComponent } from './search_result/search_result.component';
-import { SearchResultProcurementComponent } from './search_result/search_result.component';
-import { SearchResultSupportsComponent } from './search_result/search_result.component';
-import { SearchResultEntitiesComponent } from './search_result/search_result.component';
-import { SearchResultPeopleComponent } from './search_result/search_result.component';
+import { SearchResultComponent } from './search_result/search_result.component';
 
-import { Highlighter } from './highlighter/search.highlighter';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -31,14 +24,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     SearchComponent,
-    SearchResultBudgetComponent,
-    SearchResultChangesComponent,
-    SearchResultExemptionComponent,
-    SearchResultProcurementComponent,
-    SearchResultSupportsComponent,
-    SearchResultEntitiesComponent,
-    SearchResultPeopleComponent,
-    Highlighter
+    SearchResultComponent
   ],
   providers: [
     SearchService
--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by adam on 18/12/2016.
  */
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable }        from 'rxjs/Observable';
 import { Subject }           from 'rxjs/Subject';
 import { BehaviorSubject }   from 'rxjs/BehaviorSubject';
@@ -9,6 +9,7 @@ import { SearchService }     from '../_service/search.service';
 import { SearchResults, DocResultEntry, SearchResultsCounter} from '../_model/SearchResults';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import {HostListener} from '../../node_modules/@angular/core/src/metadata/directives';
 
 @Component({
   selector: 'budget-search',
@@ -25,19 +26,12 @@ export class SearchComponent implements OnInit {
   private allResults: any;
   private resultTotal: number;
   private resultTotalCount: SearchResultsCounter;
-  private resultCurrentCount: SearchResultsCounter;
   private displayDocs: string; // category
-  private currentDocs: string; // category
   private pageSize: number; // how many records to load for each scroll
-  private skip: number;  // how many records to skip from recently fetched query (when appending to the list)
-  private fetchFlag: boolean;
-  private resultRenew: boolean; //
+  private bump: boolean;
   private headerBottomBorder: boolean;
   private isSearching: boolean;
   private isErrorInLastSearch: boolean;
-
-  @ViewChild('searchBody')
-  private searchBodyEl: ElementRef;
 
   constructor(
     private searchService: SearchService,
@@ -49,17 +43,14 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
     this.searchTerms = new Subject<string>();
     this.allDocs = new BehaviorSubject<DocResultEntry[]>([]);
-    this.allResults = [];
-    this.resultTotal = 0;
-    this.resultTotalCount = new SearchResultsCounter();
-    this.resultCurrentCount = new SearchResultsCounter();
-    this.displayDocs = 'all';
-    this.currentDocs = 'all';
+
+    this.displayDocs = null;
+    this.bump = false;
+
+    this.resetState('all');
+
     this.pageSize = 10;
-    this.skip = -10;
-    this.fetchFlag = true;
-    this.resultRenew = true;
-    this.allResults = [];
+
     this.headerBottomBorder = false;
     this.isSearching = false;
     this.isErrorInLastSearch = false;
@@ -79,6 +70,7 @@ export class SearchComponent implements OnInit {
       .catch(error => {
         this.isSearching = false;
         this.isErrorInLastSearch = true;
+        this.displayDocs = null;
         console.log('Error while searching:', error);
         return Observable.of<SearchResults>(null);
       });
@@ -89,8 +81,8 @@ export class SearchComponent implements OnInit {
 
     this.route.queryParams
       .subscribe((params: Params) => {
-        if (params.term) {
-          this.search(params.term);
+        if (params['term']) {
+          this.search(params['term']);
         }
         return null;
       });
@@ -100,20 +92,11 @@ export class SearchComponent implements OnInit {
    * Push a search term into the observable stream.
    */
   search(term: string): void { // keyUp()
-    this.isSearching = true;
     if (this.term !== term) { // initiate a new search
-      this.pageSize = 10;
-      this.skip = -10;
-      this.resultRenew = true;
-      this.fetchFlag = true;
       this.term = term;
-      this.searchTerms.next(term);
-      this.doSwitchTab(1, 'all');
-      this.allResults = [];
-
+      this.displayDocs = null;
+      this.resetState('all');
     } else {
-      this.resultRenew = false;
-      this.term = term;
       this.searchTerms.next(term);
     }
   }
@@ -125,45 +108,11 @@ export class SearchComponent implements OnInit {
    */
 
   doRequest(): Observable<SearchResults> {
-    this.currentDocs = this.displayDocs;
-    let maxRecords = 0;
-    if (this.resultRenew) {
-      maxRecords = 11;
-    } else if (this.displayDocs === 'all') {
-      let result_arr = this.resultTotalCount;
-      let count_arr = Object.keys(result_arr)
-        .map(key => {
-          return result_arr[key];
-        });
-      maxRecords = Math.max(...count_arr, 21);
-    } else {// if specific category is selected - maxRecords is the totalCount of that category(currentDocs)
-      maxRecords = this.resultTotalCount[this.currentDocs];
-    }
-
-    if (this.pageSize + this.skip < maxRecords) {
-      this.skip += this.pageSize;
-    } else if (this.pageSize + this.skip < maxRecords && maxRecords !== 0) {
-      this.skip = maxRecords - this.pageSize;
-    } else if (this.resultCurrentCount[this.displayDocs] < 10 && this.resultCurrentCount[this.displayDocs] < maxRecords) {
-      this.skip = 0;
-    } else {
-      return Observable.of<SearchResults>(null);
-    }
-
-    let category = [this.currentDocs];
-    if (this.resultRenew || category[0] === 'all') {
-      category = ['budget', 'national-budget-changes', 'contract-spending', 'entities'];
-      // category = ['all'];
-    // } else if (category[0] === 'procurement') {
-    //   category = ['contract-spending', 'exemptions'];
-    } else if (category[0] === 'nationalbudgetchanges') {
-      category = ['national-budget-changes'];
-    }
     if (this.term) {
       this.isSearching = true;
       this.isErrorInLastSearch = false;
-      return this.searchService.search(this.term, this.pageSize, this.skip, category);
-      } else {
+      return this.searchService.search(this.term, this.pageSize, this.allResults.length, this.displayDocs.split(','));
+    } else {
       this.isSearching = false;
       return Observable.of<SearchResults>(null);
     }
@@ -175,39 +124,41 @@ export class SearchComponent implements OnInit {
    * @param {SearchResults} results - returned results from query
    */
   processResults(results: SearchResults): void {
-    console.log('results: ', results);
     if (results) {
-      if (this.resultRenew) {
+      if (this.displayDocs === 'all') {
         this.resultTotal = 0;
-        this.resultTotalCount = new SearchResultsCounter();
-        this.resultCurrentCount = new SearchResultsCounter();
-      }
-      for (let key in results.search_counts) {
-        if (key) {
-          let tmpResults = results.search_counts[key];
-          if (key === 'exemptions' || key === 'contractspending') {
-            key = 'procurement';
-          }
-          if (this.resultRenew) {
+        for (let key in results.search_counts) {
+          if (key) {
+            let tmpResults = results.search_counts[key];
             this.resultTotal += tmpResults.total_overall;
-            this.resultTotalCount[key] += tmpResults.total_overall;
+            this.resultTotalCount[key] = tmpResults.total_overall;
           }
         }
-      }
-      for (let item of results.search_results){
-        let key = item.type;
-        if (key === 'exemptions' || key === 'contractspending') {
-          key = 'procurement';
-        }
-        this.resultCurrentCount[key] += 1;
       }
       this.allResults.push(...results.search_results);
-      this.allDocs.next(this.allResults);
-      this.fetchFlag = true;
-      this.resultRenew = false;
     } else {
-      this.fetchFlag = false;
+      this.resetState(null);
+      this.resetState('all');
     }
+    this.allDocs.next(this.allResults);
+
+  }
+
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if ((window.innerHeight * 1.7 + window.scrollY) >= document.body.scrollHeight) {
+      // Scrolled to bottom
+      if (!this.bump) {
+        this.bump = true;
+        this.fetchMore();
+      }
+    } else {
+      if (this.bump) {
+        this.bump = false;
+      }
+    }
+
   }
 
   /**
@@ -215,19 +166,10 @@ export class SearchComponent implements OnInit {
    * when scrolling, appends more results to the list
    * @param {number} term
    */
-  fetchMore(term: number): void {
-    const div = document.body.getElementsByClassName('search_body')[0];
+  fetchMore(): void {
     this.headerBottomBorder = true;
-    const cur = div.scrollTop;
-    const divHeight = div.scrollHeight;
-    if (this.currentDocs !== this.displayDocs) {
-      this.currentDocs = this.displayDocs;
-      this.fetchFlag = true;
-    }
-    if (cur > 0.3 * divHeight && this.fetchFlag) {
-      this.fetchFlag = false;
-      this.searchTerms.next(this.term);
-    }
+    this.isSearching = true;
+    this.searchTerms.next(this.term);
   }
 
   getStatusText() {
@@ -242,21 +184,25 @@ export class SearchComponent implements OnInit {
     return '';
   }
 
-  switchTab($event: any, collectionTotal: number, docType: string) {
-    $event.stopPropagation();
-    $event.preventDefault();
-
-    this.doSwitchTab(collectionTotal, docType);
-  }
-
-  doSwitchTab(collectionTotal: number, docType: string) {
-    if (collectionTotal) {
-      this.displayDocs  = docType;
-      this.searchBodyEl.nativeElement.scrollTop = 0;
-      if (this.resultCurrentCount[docType] < 10) {
+  resetState(requestedDocTypes: string) {
+    if (requestedDocTypes !== this.displayDocs) {
+      this.displayDocs = requestedDocTypes;
+      this.allResults = [];
+      if (requestedDocTypes === 'all') {
+        this.resultTotal = 0;
+        this.resultTotalCount = new SearchResultsCounter();
+      }
+      if (this.displayDocs && this.term) {
+        this.isSearching = true;
         this.searchTerms.next(this.term);
       }
     }
+  }
 
+  switchTab($event: any, requestedDocTypes: string) {
+    $event.stopPropagation();
+    $event.preventDefault();
+
+    this.resetState(requestedDocTypes);
   }
 }
