990bb63ae4c72b9ed8697baa68b07558d81aee9d
Ori Hoch <ori@uumpa.com>
2018-01-08 21:18:17 +0200

    added budgetkey-ng2-auth provider (#97)

--- index.js
+++ index.js
@@ -75,7 +75,10 @@ app.get(basePath + '*', function(req, res) {
     title = 'חיפוש במפתח התקציב: ' + kind + term
   }
 
-  res.render('index.html', {themeScript: themeScript, base: basePath, title: title});
+  res.render('index.html', {
+    themeScript: themeScript, base: basePath, title: title,
+    authServerUrl: process.env.AUTH_SERVER_URL
+  });
 });
 
 app.listen(app.get('port'), function() {
