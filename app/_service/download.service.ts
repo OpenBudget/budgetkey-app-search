8cb8345c5096c2a0b41dfd330c77ce9f5728ae80
yontilevin <yontilevin@gmail.com>
2018-01-22 23:58:48 +0200

    travis fixes

--- app/_service/download.service.ts
+++ app/_service/download.service.ts
@@ -9,17 +9,17 @@ import { DocResultEntry } from '../_model/SearchResults';
 @Injectable()
 export class DownloadService {
 
-  exportAsCsv(filename: string, allDocs: BehaviorSubject<DocResultEntry[]>): void{
-    var addTitle = function (type:string, hdrs: Array<string>){
+  exportAsCsv(filename: string, allDocs: BehaviorSubject<DocResultEntry[]> ): void {
+    let addTitle = function (type:string, hdrs: Array<string>){
       console.log(type);
-      var title = type + '\n' + 'type';
+      let title = type + '\n' + 'type';
 
       for (let h of hdrs){
         console.log(h);
         title += ',' + h;
       }
-      return title + '\n'
-    }
+      return title + '\n';
+    };
     // var processRow = function (row) {
     //     var finalVal = '';
     //     for (var j = 0; j < row.length; j++) {
@@ -36,53 +36,53 @@ export class DownloadService {
     //     }
     //     return finalVal + '\n';
     // };
-    var csvFile = '';
-    var headers4type = {'budget': ['title', 'page_title', 'amounts_allocated', 'amounts_executed', 'amounts_revised', 'explanation',
+    let csvFile = '';
+    let headers4type = {'budget': ['title', 'page_title', 'amounts_allocated', 'amounts_executed', 'amounts_revised', 'explanation',
                                    'explanation_source', 'nice-breadcrumbs', 'hierarchy'],
                         'entities': ['name', 'page_title', 'received_amount', 'address lines',
                                      'details', 'description', 'goal', 'government'],
                         'nationalbudgetchanges': ['explanation', 'page_title', 'budget_code_title', 'amount'],
-                        'supports': ['amount_total', 'entity_kind', 'entity_name', 'page_title', 'request_type', 'year_requested'],
+                        'supports': ['amount_total', 'entity_kind', 'entity_name', 'page_title', 'request_type', 'year_requested'], 
                         'contractspending': ['budget_code', 'budget_title', 'buyer_description', 'end_date', 'entity_id', 
                                              'entity_kind', 'entity_name', 'executed', 'exemption_reason', 'explanation',
-                                             'page_title', 'purchase_method', 'supplier_name']}
-    var keys = []
+                                             'page_title', 'purchase_method', 'supplier_name']};
+    let keys = [];
     for (let key of allDocs.value){
-      keys.push(key.type)
+      keys.push(key.type);
     }
 
-    let unique =  keys.filter((v, i, a) => a.indexOf(v) === i && v != 'tenders');
-    console.log(unique)
+    let unique =  keys.filter((v, i, a) => a.indexOf(v) === i && v !== 'tenders');
+    console.log(unique);
 
     for (let key of unique){
-      var headers = headers4type[key]
-      csvFile += addTitle(key, headers);
+      let headers = headers4type[key]
+      csvFile += addTitle(key, headers); 
       for (let entry of allDocs.value){
-        if (entry.type == key){
-          csvFile += key 
+        if (entry.type === key) {
+          csvFile += key; 
           for (let h of headers){
-            csvFile += ',' + entry.source[h]
+            csvFile += ',' + entry.source[h]; 
           }
-          csvFile += '\n'
+          csvFile += '\n'; 
         }
       }
-      csvFile += '\n\n'
+      csvFile += '\n\n'; 
     }
     // var csvFile = '';
     // // for (var i = 0; i < rows.length; i++) {
     // //     csvFile += processRow(rows[i]);
     // // }
     // // csvFile += results
-    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    let blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
     if (navigator.msSaveBlob) { // IE 10+
         navigator.msSaveBlob(blob, filename);
     } else {
-        var link = document.createElement("a");
+        let link = document.createElement('a');
         if (link.download !== undefined) { // feature detection
             // Browsers that support HTML5 download attribute
             var url = URL.createObjectURL(blob);
-            link.setAttribute("href", url);
-            link.setAttribute("download", filename);
+            link.setAttribute('href', url);
+            link.setAttribute('download', filename);
             link.style.visibility = 'hidden';
             document.body.appendChild(link);
             link.click();
