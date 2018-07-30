5304fdad6da04c788391107db1517dd6318ed6fa
Simon Borsky <simonbor@gmail.com>
2018-07-31 00:18:30 +0300

    static data ui rendering

--- app/timeline-scale/timeline-scale.component.ts
+++ app/timeline-scale/timeline-scale.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+const data = require('./timeline-scale.data.json');
 
 @Component({
   selector: 'timeline-scale',
   template: require('./timeline-scale.component.html'),
   styles: [require('./timeline-scale.component.css')]
 })
+
 export class TimelineScaleComponent implements OnInit {
+
+  private timeline = data.timeline;
+  private coeficient = 0;
 
   constructor() { }
 
   ngOnInit() {
+
+    let maxValue = 0
+    this.timeline.forEach((item:any, index:any) => {
+      maxValue = item[1] > (maxValue || 0) ? item[1] : maxValue;
+    });
+
+    this.coeficient = 100 / maxValue;
   }
 
 }
