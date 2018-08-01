cad3bce0dc21cce1501f7af2621ace124ea9ea68
Adam Kariv <adam.kariv@gmail.com>
2018-08-01 15:59:41 +0300

    New design of login request modal

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,7 +9,8 @@ import { HttpModule } from '@angular/http';
 import { SearchService } from './_service/search.service';
 import { SearchComponent } from './search/search.component';
 
-import { BudgetKeyCommonModule, THEME_TOKEN as NG_COMPONENTS_THEME_TOKEN } from 'budgetkey-ng2-components';
+import { BudgetKeyCommonModule, 
+         THEME_ID_TOKEN, THEME_TOKEN } from 'budgetkey-ng2-components';
 
 import { SearchResultComponent,
          BudgetSearchResultComponent,
@@ -28,27 +29,21 @@ import { SearchFilterMenuBarComponent,
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { InjectionToken } from '@angular/core';
-import { THEME_ID_TOKEN } from './_config/config';
-
-let defaultTheme = {
-  // TODO: add default theme values
-};
-
-const THEME_TOKEN = new InjectionToken('Theme Config');
-declare const BUDGETKEY_NG2_COMPONENTS_THEME: any;
+declare let BUDGETKEY_NG2_COMPONENTS_THEME: any;
 declare const BUDGETKEY_APP_SEARCH_THEME: any;
 declare const BUDGETKEY_THEME_ID: any;
 
-declare const authServerUrl: any;
-
 let providers: any[] = [
   SearchService,
-  {provide: THEME_TOKEN, useValue: typeof(BUDGETKEY_APP_SEARCH_THEME) === 'undefined' ? defaultTheme : BUDGETKEY_APP_SEARCH_THEME},
   {provide: THEME_ID_TOKEN, useValue: typeof(BUDGETKEY_THEME_ID) === 'undefined' ? null : BUDGETKEY_THEME_ID}
 ];
+
 if (typeof(BUDGETKEY_NG2_COMPONENTS_THEME) !== 'undefined') {
-  providers.push({provide: NG_COMPONENTS_THEME_TOKEN, useValue: BUDGETKEY_NG2_COMPONENTS_THEME});
+  if (typeof(BUDGETKEY_APP_SEARCH_THEME) !== 'undefined') {
+    BUDGETKEY_NG2_COMPONENTS_THEME = Object.assign({}, BUDGETKEY_NG2_COMPONENTS_THEME, BUDGETKEY_APP_SEARCH_THEME);
+  }
+  providers.push({provide: THEME_TOKEN,
+                  useValue: BUDGETKEY_NG2_COMPONENTS_THEME});
 }
 
 @NgModule({
