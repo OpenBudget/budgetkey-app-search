cfc860434c9bc60833a035a3cb7593c62104d17b
Adam Kariv <adam.kariv@gmail.com>
2018-08-01 13:55:30 +0300

    Spending result style fixes

--- app/search-result/per-entity/contract-spending.search-result.component.ts
+++ app/search-result/per-entity/contract-spending.search-result.component.ts
@@ -10,8 +10,12 @@ import { BaseSearchResultComponent } from './base.search-result.component';
   template: require('./spending.search-result.component.html')
 })
 export class ContractSpendingSearchResultComponent extends BaseSearchResultComponent {
-  tag () {
+  tag() {
     return 'התקשרות';
+  }
+
+  tag2() {
+    return this.d['contract_active'] ? 'פעילה' : 'הסתיימה';
   }
 
   title() {
