b7996acdec713125bf44ce9478712f32f4b34d35
Adam Kariv <akariv@users.noreply.github.com>
2019-04-01 13:16:47 +0300

    Redesign search cards (#131)
    * Redesign search cards
    
    * more deletes
    
    * lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,6 @@ import { SearchComponent } from './search/search.component';
 import { SearchFilterMenuComponent } from './search-filter-menu/search-filter-menu.component';
 import { SearchFilterMenuBarComponent } from './search-filter-menu-bar/search-filter-menu-bar.component';
 import { SearchResultComponent } from './search-result/search-result.component';
-import { SearchResultBudgetComponent } from './search-result-budget/search-result-budget.component';
-import { SearchResultContractSpendingComponent } from './search-result-contract-spending/search-result-contract-spending.component';
-import { SearchResultEntitiesComponent } from './search-result-entities/search-result-entities.component';
-import { SearchResultReportsComponent } from './search-result-reports/search-result-reports.component';
-import { SearchResultSupportsComponent } from './search-result-supports/search-result-supports.component';
-import { SearchResultTendersComponent } from './search-result-tenders/search-result-tenders.component';
 import { TimelineMenuComponent } from './timeline-menu/timeline-menu.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -53,12 +47,6 @@ if (typeof(BUDGETKEY_NG2_COMPONENTS_THEME) !== 'undefined') {
     SearchFilterMenuComponent,
     SearchFilterMenuBarComponent,
     SearchResultComponent,
-    SearchResultBudgetComponent,
-    SearchResultContractSpendingComponent,
-    SearchResultEntitiesComponent,
-    SearchResultReportsComponent,
-    SearchResultSupportsComponent,
-    SearchResultTendersComponent,
     TimelineMenuComponent,
     VerticalResultsComponent,
     SearchModeSelectorComponent,
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -3,7 +3,40 @@ import { Component, Input, Inject, OnInit } from '@angular/core';
 import { THEME_ID_TOKEN } from 'budgetkey-ng2-components';
 
 import { DocResultEntry } from '../model';
-import { KIND_PARAMETERS } from '../kind-parameters';
+
+import * as moment from 'moment';
+
+type StringOrFunc = string | ((any) => string);
+
+interface Parameter {
+  // Colors:
+  primaryColor: StringOrFunc;
+  secondaryColor: StringOrFunc;
+  tertiaryColor?: StringOrFunc;
+  bgColor: StringOrFunc;
+
+  // Top line:
+  tag?: StringOrFunc;
+  postTag?: StringOrFunc;
+  preAmount?: StringOrFunc;
+  amount?: StringOrFunc;
+
+  // Main body:
+  bodyStyle?: StringOrFunc;
+  title?: StringOrFunc;
+  partyFrom?: StringOrFunc;
+  partyTo?: StringOrFunc;
+  arrowKind?: StringOrFunc;
+  mainId?: StringOrFunc;
+  rightIcon?: StringOrFunc;
+  leftIcon?: StringOrFunc;
+
+  // Bottom line:
+  bottomLineText?: StringOrFunc;
+  bottomLineLabel?: StringOrFunc;
+  bottomLineLabelStyle?: StringOrFunc;
+  bottomLineTextOpacity?: StringOrFunc;
+}
 
 
 @Component({
@@ -16,17 +49,437 @@ export class SearchResultComponent implements OnInit {
   @Input() item: DocResultEntry;
   @Input() index: number;
   @Input() kind: string;
-  @Input() mobileView = false;
+  @Input() horizontal = false;
 
-  public parameters: any;
+  private PARAMETERS: { [s: string]: Parameter; } = {
+    // // ENTITIES
+    // Companies
+    'org/company': <Parameter>{
+      primaryColor: '#3e4e59',
+      secondaryColor: '#e8f5ff',
+      tertiaryColor: '#abbbc6',
+      bgColor: '#ffffff',
+      tag: ':kind_he',
+      preAmount: () => `הכנסות&nbsp;מהמדינה ${this.threeYears()}&nbsp;-&nbsp;${this.thisYear()}`,
+      amount: (x) => `${this.format_number(x.received_amount)} ₪`,
+
+      // Main body:
+      title: ':name',
+      mainId: '#:id',
+
+      // Bottom line:
+      bottomLineText: (x: any) => (x.details.description ?
+        `<strong>מטרות</strong>: ${x.details.description}` :
+        (x.details.goals ?
+          `<strong>מטרות</strong>: ${x.details.goals}` :
+          (x.details.address_lines ?
+            `<strong>כתובת</strong>: ${x.details.address_lines.join(', ')}` : null
+          )
+        )
+      )
+    },
+    // Associations
+    'org/association': <Parameter>{
+      primaryColor: '#235000',
+      secondaryColor: '#eaf9de',
+      tertiaryColor: '#abba9f',
+      bgColor: '#ffffff',
+      tag: ':kind_he',
+      preAmount: (x) => (
+        this.socialmapAmount() ?
+        (x.details['last_report_year'] ?
+          `מחזור&nbsp;כספי&nbsp;שנתי לשנת&nbsp;${x.details['last_report_year']}` :
+          `מחזור&nbsp;כספי&nbsp;שנתי לשנת&nbsp;הדיווח&nbsp;האחרונה`
+        ) :
+        (`הכנסות&nbsp;מהמדינה ${this.threeYears()}&nbsp;-&nbsp;${this.thisYear()}`)
+      ),
+      amount: (x) => (
+        this.socialmapAmount() ?
+        (`${this.format_number(x.details.yearly_turnover)} ₪`) :
+        (`${this.format_number(x.received_amount)} ₪`)
+      ),
+
+      // Main body:
+      title: ':name',
+      mainId: '#:id',
+
+      // Bottom line:
+      bottomLineText: (x: any) => (x.details.objective ?
+        `<strong>מטרות</strong>: ${x.details.objective}` :
+        (x.details.activity_region_list ?
+          `<strong>איזורי פעילות</strong>: ${x.details.activity_region_list.join(', ')}` :
+          (x.details.address_lines ?
+            `<strong>כתובת</strong>: ${x.details.address_lines.join(', ')}` : null
+          )
+        )
+      )
+    },
+    // Municipalities
+    'org/municipality': <Parameter>{
+      primaryColor: '#564a2a',
+      secondaryColor: '#fef2d2',
+      tertiaryColor: '#564a2a',
+      bgColor: '#ffffff',
+      tag: ':kind_he',
+      postTag: ':details.status_municipal_2015',
+      preAmount: () => `הכנסות&nbsp;מהמדינה ${this.threeYears()}&nbsp;-&nbsp;${this.thisYear()}`,
+      amount: (x) => `${this.format_number(x.received_amount)} ₪`,
+
+      // Main body:
+      title: ':name',
+      mainId: '#:id',
+
+      // Bottom line:
+      bottomLineText: (x: any) =>
+        `אזור ${x.details.district_2015} | ` +
+        `${this.format_number(x.details.total_population_end_2015_1000s * 1000)} תושבים | ` +
+        `אשכול ${x.details.index_socioeconomic_2013_cluster_from_1_to_10_1_lowest_most}/10`
+    },
+    // Other entities
+    'org': <Parameter>{
+      primaryColor: '#444',
+      secondaryColor: '#eee',
+      tertiaryColor: '#444',
+      bgColor: '#ffffff',
+      tag: ':kind_he',
+      preAmount: () => `הכנסות&nbsp;מהמדינה ${this.threeYears()}&nbsp;-&nbsp;${this.thisYear()}`,
+      amount: (x) => `${this.format_number(x.received_amount)} ₪`,
+
+      // Main body:
+      title: ':name',
+      mainId: '#:id',
+    },
+    // // BUDGET ITEMS
+    'budget': <Parameter>{
+      // Colors:
+      // .func-cls-1 { .budget-func-cls-color(#5C7899, #6092CC, #D7E7FA, #EBF2FA); } //Blue
+      // .func-cls-2 { .budget-func-cls-color(#7A6B99, #9281B8, #E4DCF5, #F1EDF7); } //Purple
+      // .func-cls-3 { .budget-func-cls-color(#6D8F89, #75BFB3, #CEF0EB, #EBF7F5); } //Cyan
+      // .func-cls-4 { .budget-func-cls-color(#A67453, #F0A16C, #FFD1B3, #FCF0E8); } //Orange
+      // .func-cls-5 { .budget-func-cls-color(#768275, #8EA38C, #D4E3D1, #ECF2EB); } //Greenish
+      // .func-cls-6 { .budget-func-cls-color(#998282, #C7B3B3, #EDE4E4, #F7F2F2); } //Sepia
+      // .func-cls-7 { .budget-func-cls-color(#8C8231, #F2E22E, #FFF7B3, #FCFADC); } //Yellow
+      // .func-cls-8 { .budget-func-cls-color(#313131, #2E2E2E, #B3B3B3, #DCDCDC); } //Yellow
+      primaryColor: '#7a6b99',
+      secondaryColor: '#e4dcf5',
+      tertiaryColor: '#b4a0de',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: (x) => x.code[0] === 'C' ? 'נושא תקציבי' : 'סעיף תקציבי',
+      amount: (x) => `${this.format_number(x.net_revised)}&nbsp;₪`,
+
+      // Main body:
+      title: ':title',
+      mainId: ':nice-code',
+
+      // Bottom line:
+      bottomLineText: ':nice-breadcrumbs',
+      bottomLineLabel: ':nice-short-category',
+      bottomLineLabelStyle: 'circle'
+    },
+    // // TRANSACTIONS
+    // Contracts:
+    'contract-spending': <Parameter>{
+      // Colors:
+      primaryColor: '#19008f',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffefc',
+
+      // Top line:
+      tag: 'התקשרות רכש',
+      postTag: (x) => x.contract_is_active ? 'פעילה' : 'הסתיימה',
+      amount: (x) => `${this.format_number(x['volume'])}&nbsp;₪`,
+
+      // Main body:
+      bodyStyle: 'ticket',
+      title: ':purpose',
+      partyFrom: ':publisher_name',
+      partyTo: (x) => x['entity_name'] || x['supplier_name'][0],
+      arrowKind: 'left',
+
+      // Bottom line:
+      bottomLineText: (x) => this.contractPeriodDetails(x),
+      bottomLineTextOpacity: '0.5',
+    },
+    // Supports:
+    'supports': <Parameter>{
+      // Colors:
+      primaryColor: '#235000',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffefc',
+
+      // Top line:
+      tag: 'תמיכה תקציבית',
+      postTag: (x) => x['request_type'] === 'א3' ? '3' + 'א' : x['request_type'],
+      amount: (x) => `${this.format_number(x['amount_total'])}&nbsp;₪`,
+
+      // Main body:
+      bodyStyle: 'ticket',
+      title: (x) => this.lastSupportPayment(x)['support_title'],
+      partyFrom: (x) => this.lastSupportPayment(x)['supporting_ministry'],
+      partyTo: (x) => x['entity_name'] || x['recipient'],
+      arrowKind: 'left',
+
+      // Bottom line:
+      bottomLineText: (x) => this.supportPeriodDetails(x),
+      bottomLineTextOpacity: '0.5',
+    },
+    // // OPEN CALLS
+    // Exemptions:
+    'tenders/exemptions': <Parameter>{
+      // Colors:
+      primaryColor: '#19008f',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffbf2',
+
+      // Top line:
+      tag: ':tender_type_he',
+      postTag: ':simple_decision_long',
+
+      // Main body:
+      bodyStyle: 'dashing',
+      title: ':description',
+      partyFrom: ':publisher',
+      partyTo: ':awardees_text',
+      arrowKind: 'left-dashed',
+
+      // Bottom line:
+      bottomLineText: 'עודכן לאחרונה: :last_update_date',
+      // bottomLineLabel?: StringOrFunc;
+      // bottomLineLabelStyle?: StringOrFunc;
+      bottomLineTextOpacity: '0.5'
+    },
+    // Tenders:
+    'tenders': <Parameter>{
+      // Colors:
+      primaryColor: '#19008f',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffbf2',
+
+      // Top line:
+      tag: ':tender_type_he :simple_decision',
+      postTag: ':regulation',
+
+      // Main body:
+      bodyStyle: 'dashing',
+      title: ':description',
+      partyFrom: ':publisher',
+      partyTo: ':awardees_text',
+      arrowKind: 'left-dashed',
+
+      // Bottom line:
+      bottomLineText: 'עודכן לאחרונה: :last_update_date',
+      // bottomLineLabel?: StringOrFunc;
+      // bottomLineLabelStyle?: StringOrFunc;
+      bottomLineTextOpacity: '0.5'
+    },
+    // Calls for Bids:
+    'calls_for_bids': <Parameter>{
+      // Colors:
+      primaryColor: '#235000',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffbf2',
+
+      // Top line:
+      tag: ':tender_type_he',
+      postTag: ':decision',
+
+      // Main body:
+      bodyStyle: 'dashing',
+      title: ':page_title',
+      partyFrom: ':publisher',
+
+      // Bottom line:
+      bottomLineText: 'תאריך פרסום: :start_date',
+      // bottomLineLabel?: StringOrFunc;
+      // bottomLineLabelStyle?: StringOrFunc;
+      bottomLineTextOpacity: '0.5'
+    },
+    // Support Criteria:
+    'support_criteria': <Parameter>{
+      // Colors:
+      primaryColor: '#235000',
+      secondaryColor: '#fffbf2',
+      bgColor: '#fffbf2',
+
+      // Top line:
+      tag: ':tender_type_he',
+      postTag: ':decision',
+
+      // Main body:
+      bodyStyle: 'dashing',
+      title: ':page_title',
+      partyFrom: ':publisher',
+
+      // Bottom line:
+      bottomLineText: 'תאריך פרסום: :start_date',
+      // bottomLineLabel?: StringOrFunc;
+      // bottomLineLabelStyle?: StringOrFunc;
+      bottomLineTextOpacity: '0.5'
+    },
+    // // PEOPLE:
+    // People
+    'people': <Parameter>{
+      // Colors:
+      primaryColor: '#91a0c2',
+      secondaryColor: '#fafafa',
+      tertiaryColor: '#91a0c2',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: 'אנשים',
+      preAmount: (x) => x.details.length > 1 ? `${x.details.length} רשומות` : 'רשומה אחת',
+
+      // Main body:
+      title: ':key',
+      rightIcon: 'face',
+
+      // Bottom line:
+      bottomLineText: '', // TODO: Complete'
+    },
+    // // TRANSFERS:
+    // Budget Changes
+    'national-budget-changes': <Parameter>{
+      // Colors:
+      primaryColor: '#7d7d7d',
+      secondaryColor: '#f5f5f5',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: 'העברה תקציבית',
+      postTag: (x) => x.change_type_name && x.change_type_name.length > 0 ?
+        (x.committee_id && x.committee_id.length > 0 && x.committee_id[0] ?
+          x.change_type_name[0] + ' (פניה #' + x.committee_id[0] + ')' :
+          x.change_type_name[0]
+        ) : null,
+      amount: (x) => `${this.format_number(x.amount)} ₪`,
+
+
+      // Main body:
+      title: ':page_title',
+      partyFrom: (x) => x['summary']['from'].map((i) => i[2]).join(', '),
+      partyTo: (x) => x['summary']['to'].map((i) => i[2]).join(', '),
+      arrowKind: 'left-dashed',
+      mainId: '#:transaction_id',
+
+      // Bottom line:
+      bottomLineText: (x) => x['pending'] && x['pending'][0] ? '<strong>ממתינה לאישור</strong>' : (
+                        (x['date'] && x['date'].length > 0) ?
+                          `<strong>אושרה ב:</strong> ${moment(x['date'][0]).format('DD/MM/YYYY')}`
+                          : null
+      )
+    },
+    // // PUBLICATIONS
+    // Government Decisions
+    'gov_decisions': <Parameter>{
+      // Colors:
+      primaryColor: '#19008f',
+      secondaryColor: '#ebe9f5',
+      tertiaryColor: '#a4a1b3',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: ':policy_type',
+      postTag: (x) => x['unit'] && x['unit'].length < 36 ? ':unit' : null,
+      preAmount: (x) => moment(x['publish_date']).format('DD/MM/YYYY'),
+
+      // Main body:
+      title: ':title',
+      mainId: (x) => x['procedure_number_str'] ? '#:procedure_number_str' : null,
+      leftIcon: 'emblem-of-israel',
+
+      // Bottom line:
+      bottomLineText: '<strong>מפרסם:</strong> :office'
+    },
+    // // REPORTS
+    // NGO Activity Report
+    'reports/ngo-activity-report': <Parameter>{
+      // Colors:
+      primaryColor: '#180a42',
+      secondaryColor: '#fe8255',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: 'תחום פעילות',
+
+      // Main body:
+      title: ':title',
+
+      // Bottom line:
+      bottomLineText: '<strong>מספר ארגונים:</strong> :details.report.total.total_amount',
+    },
+    // District Report
+    'reports/ngo-district-report': <Parameter>{
+      // Colors:
+      primaryColor: '#180a42',
+      secondaryColor: '#fe8255',
+      bgColor: '#ffffff',
+
+      // Top line:
+      tag: 'אזור פעילות',
+
+      // Main body:
+      title: ':title',
+
+      // Bottom line:
+      bottomLineText: '<strong>מספר ארגונים:</strong> :details.report.total.count',
+    },
+
+  };
+
+  public p: Parameter;
 
   constructor(@Inject(THEME_ID_TOKEN) private theme_id: any) { }
   ngOnInit() {
-    for (const parameters of KIND_PARAMETERS) {
-      if (this.item.source.doc_id.startsWith(parameters.docType)) {
-        this.parameters = parameters;
-        break;
+    const parts = this.item.source.doc_id.split('/');
+    this.p = <Parameter>{};
+    const template = (
+      this.PARAMETERS[parts[0] + '/' + parts[1]] ||
+      this.PARAMETERS[parts[0]] ||
+      this.PARAMETERS['']
+    );
+    if (template) {
+      for (const el of Object.entries(template)) {
+        const k = el[0];
+        let v = el[1];
+        if (typeof v === 'function') {
+          v = v(this.item.source);
+        }
+        if (typeof v === 'string') {
+          v = v.replace(/:([-a-z0-9_.]+)/g, (_, x) => this.get(x));
+        } else {
+          v = null;
+        }
+        this.p[k] = v;
       }
+    }
+  }
+
+
+  threeYears() {
+    const dt = new Date();
+    dt.setDate(dt.getDate() - 180);
+    return dt.getFullYear() - 3;
+  }
+
+  thisYear() {
+    const dt = new Date();
+    return dt.getFullYear();
+  }
+
+  format_number(x: number) {
+    if (x) {
+      let fracDigits = 0;
+      if (x < 1000) {
+        fracDigits = 2;
+      }
+      return '<span class="number">' +
+                x.toLocaleString('en-US', {style: 'decimal',
+                                           maximumFractionDigits: fracDigits}) +
+             '</span>';
+    } else {
+      return '-';
     }
   }
 
@@ -35,17 +488,76 @@ export class SearchResultComponent implements OnInit {
       const part = parts.shift();
       item = item[part];
     }
+    console.log('returning', item);
     return item;
   }
 
   get(field: string, default_value?: string) {
+    console.log('getting', field);
     if (field) {
       return this.get_field(this.item.source, field.split('.')) || default_value || '';
     }
     return default_value || '';
   }
 
-  href(doc_id: string) {
-    return 'http://next.obudget.org/i/' + doc_id + '?li=' + this.index + (this.theme_id ? '&theme=' + this.theme_id : '');
+  fadeoff() {
+    return `linear-gradient(to left, ${this.p.secondaryColor}00 0%, ${this.p.secondaryColor}ff 100%)`;
   }
+
+  socialmapAmount() {
+    return this.theme_id === 'socialmap' && this.item.source.details['yearly_turnover'];
+  }
+
+  contractPeriodDetails(x) {
+    const r: string[] = [];
+    if (x['order_date']) {
+      r.push('תקופת ההתקשרות');
+      let rr = x['order_date'] + ' - ';
+      if (x['end_date']) {
+        rr += x['end_date'];
+      } else if (x['contract_is_active']) {
+        rr += 'התקשרות פעילה';
+      } else {
+        rr += 'התקשרות לא פעילה';
+      }
+      r.push(rr);
+    } else if (x['contract_is_active']) {
+      r.push('התקשרות פעילה');
+    } else {
+      r.push('התקשרות לא פעילה');
+    }
+    return r.join(' ');
+  }
+
+  supportPeriodDetails(x) {
+    const lastYear = x['last_payment_year'];
+    if (!lastYear || lastYear === x['year_requested']) {
+      return `שנת תמיכה: ${x['year_requested']}`;
+    } else {
+      return `תקופת תמיכה: ${x['year_requested']} - ${lastYear}`;
+    }
+  }
+
+  lastSupportPayment(x) {
+    return x['payments'][x['payments'].length - 1];
+  }
+
+  href() {
+    const doc_id = this.item.source.doc_id;
+    return 'https://next.obudget.org/i/' + doc_id + '?li=' + this.index + (this.theme_id ? '&theme=' + this.theme_id : '');
+  }
+
+  navigate() {
+    window.location.href = this.href();
+  }
+
+  // func_cls() {
+  //   try {
+  //     return JSON.parse(this.d['func_cls_json'][0])[0][0];
+  //   } catch (e) {
+  //     console.log('WTF with ', this.d['func_cls_json']);
+  //     return 'x';
+  //   }
+  // }
+
 }
